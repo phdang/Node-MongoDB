aab3771a679087946e82029d3bea62cead39729e
phdang <phdang94@yahoo..com>
2018-10-16 20:45:56 +0700

    Add PATCH /todos/:id

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -1,3 +1,5 @@
+const _ = require("lodash");
+
 const { ObjectID } = require("mongodb");
 
 const { Todo } = require("../models/Todo");
@@ -78,5 +80,64 @@ module.exports = app => {
           res.status(404).send({ error });
         });
     }
+  });
+  app.patch("/todos/:id", (req, res) => {
+    var todoId = req.params.id;
+
+    // first solution using lodash
+
+    // var body = _.pick(req.body, ["text", "completed"]);
+    // if (!ObjectID.isValid(todoId)) {
+    //   console.log("Todo Id is invalid");
+    //   res.status(404).send({ error: "Todo Id not found !" });
+    // } else {
+    //   if (_.isBoolean(body.completed) && body.completed) {
+    //     body.completedAt = new Date().getTime();
+    //   } else {
+    //     body.completed = false;
+    //     body.completedAt = null;
+    //   }
+    //   Todo.findByIdAndUpdate(todoId, { $set: body }, { new: true })
+    //     .then(todo => {
+    //       if (!todo) {
+    //         res.status(404).send({ error });
+    //       } else {
+    //         res.status(200).send({ todo });
+    //       }
+    //     })
+    //     .catch(error => {
+    //       res.status(404).send({ error });
+    //     });
+    // }
+
+    //Second Solution
+    if (!ObjectID.isValid(todoId)) {
+      console.log("Todo Id is invalid");
+      res.status(404).send({ error: "Todo Id not found !" });
+    }
+    const newTodo = new Todo();
+    newTodo._id = ObjectID(todoId);
+    newTodo.text = req.body.text;
+    newTodo.completed = req.body.completed === true ? true : false;
+    if (newTodo.completed) {
+      newTodo.completedAt = new Date().getTime();
+    } else {
+      newTodo.completedAt = null;
+    }
+    Todo.findById(todoId)
+      .then(todo => {
+        todo.set(newTodo);
+        todo.save((error, newUpdate) => {
+          if (error) {
+            res.status(404).send({ error });
+          }
+          console.log("Update Todo");
+          console.log(JSON.stringify(newUpdate, null, 2));
+          res.status(200).send({ newUpdate });
+        });
+      })
+      .catch(error => {
+        res.status(404).send({ error });
+      });
   });
 };
